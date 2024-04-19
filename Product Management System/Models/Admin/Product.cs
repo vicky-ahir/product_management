3fc43416ec58b76cbfec1f3d2ebdea36371ad731
vicky-ahir <vickyyadav3047@gmail.com>
2024-04-19 22:54:57 +0530

    other changes

--- Product Management System/Models/Admin/Product.cs
+++ Product Management System/Models/Admin/Product.cs
@@ -5,13 +5,19 @@ namespace Product_Management_System.Models.Admin
     public class Product
     {
         public int Product_Id { get; set; }
+        [Required]
         public string Product_Name { get; set; }
+        [Required]
         public string Product_Description { get; set; }
+        [Required]
         public decimal Product_Price { get; set; }
+        [Required]
         public IFormFile? Cover_Image { get; set; }
         public string? Product_Cover_Image { get; set; }
+        [Required]
         public List<IFormFile>? Images { get; set; }
         public string? Product_Images { get; set; }
+        [Required]
         public int? Product_Quantity { get; set; }
         public int? Product_Status { get; set; }
         public int? Is_Deleted { get; set; }
